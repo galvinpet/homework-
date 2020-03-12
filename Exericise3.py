eef5a8217375d565c7f6e304d965f96eee0fd393
unknown <galvinpetrez@gmailcom>
2020-03-13 02:01:12 +0300

    Exercise 3

---
+++ Exericise3.py
@@ -0,0 +1,9 @@
+q = 5;
+for x in range(q):
+    for k in range(x):
+        print('* ', end = '')
+    print('')
+for x in range(q, 0, -1):
+    for k in range(x):
+        print('* ', end = '')
+    print('')
