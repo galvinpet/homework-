368705ce2a5ee12e14b864d3eacf2047a3b20320
unknown <galvinpetrez@gmailcom>
2020-03-13 11:58:50 +0300

    exercise 2 edited

--- Exericise2.py
+++ Exericise2.py
@@ -1,6 +1,6 @@
 #Write a Python program to find those numbers which are divisible by 7 and multiple of 5, between 1500 and 2700 (both included).
 z = []
-for a in range(1499, 2701):
+for a in range(1500, 2701):
     if (a % 7 == 0) and (a % 5):
         z.append(str(a))
 print(z)
